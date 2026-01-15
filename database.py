93a3e7d6f2eb1dd9d28c46d26bc5797e947cc09e
DaviMoreira27 <davimsantana2706@gmail.com>
2026-01-15 01:52:43 -0300

    feat: added pool connection

--- database.py
+++ database.py
@@ -1,20 +1,33 @@
 import asyncpg
 import dotenv
 import os
+import logging
+
+logger = logging.getLogger(__name__)
 
 dotenv.load_dotenv()
 
 class DatabaseService:
     def __init__(self):
-        self._database_string = os.environ["DB_STRING"]
-
-    @property
-    def database_string(self):
-        return self._database_string
+        self.database_user = os.environ['DB_USER']
+        self.database_pass = os.environ['DB_PASS']
+        self.database_host = os.environ['DB_HOST']
+        self.database_port = os.environ['DB_PORT']
+        self.database_name = os.environ['DB_NAME']
 
     async def db_connection(self):
         try:
-            return await asyncpg.connect(self.database_string)
+            conn = await asyncpg.create_pool(
+                user=self.database_user,
+                database=self.database_name,
+                password=self.database_pass,
+                port=self.database_port,
+                host=self.database_host,
+                command_timeout=60
+            )
+
+            yield
+            await conn.close()
         except (
             asyncpg.InvalidCatalogNameError,  # database does not exist
             asyncpg.InvalidAuthorizationSpecificationError,  # invalid user
@@ -23,4 +36,5 @@ class DatabaseService:
             asyncpg.CannotConnectNowError,  # server unavailable
             asyncpg.TooManyConnectionsError,  # max connections
         ) as e:
-            raise Exception("Falha interna ao conectar ao banco") from e
+            logger.error(f"Error connecting with the database: {e}")
+            raise Exception("Internal failure while connecting with the database") from e
